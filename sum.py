76d101579564b8bc16e9dbd75794b94e443df1c6
S-M-Bayezid-Hossain <bayzidhossain127@gmail.com>
2025-04-21 21:52:46 +0600

    total price calculation

---
+++ sum.py
@@ -0,0 +1,5 @@
+#in this code we calculate the sum of the price of two balls entered by the user
+ball1=int(input("Price of 1st ball: "))
+ball2=int(input("Price of 2nd ball: "))
+sum=ball1+ball2
+print(f'total price is {sum}')
